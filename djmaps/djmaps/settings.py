ec8aace5c1f4a52da33b7660eb0cd8f060d4e295
Ritik Soni <47344024+ritiksoni00@users.noreply.github.com>
2021-07-17 19:13:59 +0530

    Update settings.py

--- djmaps/djmaps/settings.py
+++ djmaps/djmaps/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'djmaps.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vwzhfvoi',
+        'USER': 'vwzhfvoi',
+        'PASSWORD': 'mxhfH_Jg_cY-ytA3NijccW-gca7J14Xk',
+        'HOST': 'hansken.db.elephantsql.com',
+        
+        'PORT': '5432',
     }
 }
 
